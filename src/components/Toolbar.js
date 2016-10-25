60c731749ebfd7a01c6fd70be9fdb05bc425a013
Johan Preynat <johan.preynat@gmail.com>
2016-10-25 17:02:19 +0200

    Add Sign in message to Toolbar component

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,13 +1,29 @@
 const GitBook   = require('gitbook-core');
 const { React } = GitBook;
 
+const getApiURL = require('../utils/getApiURL');
+
 const Toolbar = React.createClass({
     propTypes: {
-        actions: React.PropTypes.array
+        actions:  React.PropTypes.array,
+        loggedIn: React.PropTypes.bool.isRequired
     },
 
     render() {
-        const toolbarActions = this.props.actions;
+        let toolbarActions = this.props.actions;
+        const { loggedIn } = this.props;
+
+        if (!loggedIn) {
+            toolbarActions = [
+                {
+                    text: 'Sign in to GitBook.com',
+                    onClick: () => {
+                        window.location.href = getApiURL('login');
+                    }
+                }
+            ];
+        }
+
         return (
             <div className="Comment-Toolbar">
                 {toolbarActions.map((action, i) => {
@@ -28,4 +44,10 @@ const Toolbar = React.createClass({
     }
 });
 
-module.exports = Toolbar;
+function mapStateToProps({ comment }) {
+    return {
+        loggedIn: comment.get('loggedIn')
+    };
+}
+
+module.exports = GitBook.connect(Toolbar, mapStateToProps);
